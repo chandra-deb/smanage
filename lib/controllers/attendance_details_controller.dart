7d52f8473acac93e595ecfa5c9299200276eef77
chandra-deb <mail.chandradeb@gmail.com>
2021-01-08 13:38:08 +0600

    added days filtering buttons

--- lib/controllers/attendance_details_controller.dart
+++ lib/controllers/attendance_details_controller.dart
@@ -4,6 +4,8 @@ import 'package:get/get.dart';
 
 class AttendantDetailsController extends GetxController {
   AttendantDetailsController({@required this.doc});
+
+  // ** Default value is 7 days!
   RxInt daysLimit = 7.obs;
 
   final QueryDocumentSnapshot doc;
@@ -23,21 +25,36 @@ class AttendantDetailsController extends GetxController {
   String get studentName => doc.data()['name'];
   void changeLimitToSevenDays() {
     daysLimit.value = 7;
+    _changeDaysClicked();
   }
 
   void changeLimitToThirtyDays() {
     daysLimit.value = 30;
+    _changeDaysClicked();
   }
 
   void changeLimitToUnlimitedDays() {
     daysLimit.value = 365;
+    _changeDaysClicked();
+  }
+
+  Function get sevenDaysButton {
+    return sevenDaysClicked ? null : changeLimitToSevenDays;
+  }
+
+  Function get thirtyDaysButton {
+    return thirtyDaysClicked ? null : changeLimitToThirtyDays;
+  }
+
+  Function get unlimitedDaysButton {
+    return unlimitedDaysClicked ? null : changeLimitToUnlimitedDays;
   }
 
   bool sevenDaysClicked = true;
   bool thirtyDaysClicked = false;
   bool unlimitedDaysClicked = false;
 
-  void changeDaysClicked() {
+  void _changeDaysClicked() {
     if (daysLimit.value == 30) {
       thirtyDaysClicked = true;
       unlimitedDaysClicked = false;
--- lib/screens/attendance.dart
+++ lib/screens/attendance.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:smanage/controllers/attendance_details_controller.dart';
 
 class Attendance extends StatelessWidget {
@@ -7,63 +8,85 @@ class Attendance extends StatelessWidget {
   Attendance({@required this.doc});
   @override
   Widget build(BuildContext context) {
-    final attendanceDetails = AttendantDetailsController(doc: doc);
+    final _ = AttendantDetailsController(doc: doc);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Attendance of ${attendanceDetails.studentName}'),
+        title: Text('Attendance of ${_.studentName}'),
       ),
-      body: Flex(
-        direction: Axis.vertical,
-        children: [
-          Expanded(
-            flex: 1,
-            child: Container(
-              color: Colors.black,
-              child: FutureBuilder<QuerySnapshot>(
-                future: attendanceDetails.getAttendance(),
-                builder:
-                    // ignore: missing_return
-                    (BuildContext context,
-                        AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasData) {
-                    var docs = snapshot.data.docs;
-
-                    return ListView.builder(
-                      physics: BouncingScrollPhysics(),
-                      itemCount: docs.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          color: docs[index].data()['attendant'] == true
-                              ? Colors.green
-                              : Colors.red,
-                          child: ListTile(
-                            leading: Text(docs[index].id),
-                            trailing: Text(
-                                docs[index].data()['attendant'].toString()),
-                          ),
-                        ); //),
-                        // It will show dates
-                      },
-                    );
-                  }
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.grey,
-                      ),
-                    );
-                  } else {
-                    Center(
-                      child: Text(
-                          'Something Went. Connect to the Internet and try again!'),
-                    );
-                  }
-                },
+      body: Obx(() {
+        return Flex(
+          direction: Axis.vertical,
+          children: [
+            Expanded(
+              child: Container(
+                child: Row(
+                  children: [
+                    FlatButton(
+                      onPressed: _.sevenDaysButton,
+                      child: Text('Last 7 days'),
+                    ),
+                    FlatButton(
+                      onPressed: _.thirtyDaysButton,
+                      child: Text('Last 30 days'),
+                    ),
+                    FlatButton(
+                      onPressed: _.unlimitedDaysButton,
+                      child: Text('All'),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
-      ),
+            Expanded(
+              flex: 7,
+              child: Container(
+                color: Colors.black,
+                child: FutureBuilder<QuerySnapshot>(
+                  future: _.getAttendance(),
+                  builder:
+                      // ignore: missing_return
+                      (BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.hasData) {
+                      var docs = snapshot.data.docs;
+
+                      return ListView.builder(
+                        physics: BouncingScrollPhysics(),
+                        itemCount: docs.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            color: docs[index].data()['attendant'] == true
+                                ? Colors.green
+                                : Colors.red,
+                            child: ListTile(
+                              leading: Text(docs[index].id),
+                              trailing: Text(
+                                  docs[index].data()['attendant'].toString()),
+                            ),
+                          ); //),
+                          // It will show dates
+                        },
+                      );
+                    }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.grey,
+                        ),
+                      );
+                    } else {
+                      Center(
+                        child: Text(
+                            'Something Went. Connect to the Internet and try again!'),
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
