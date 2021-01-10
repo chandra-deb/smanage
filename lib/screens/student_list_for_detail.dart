c0d36e06063a90d46cf46c780f2d9e4fb7fb6d57
chandra-deb <mail.chandradeb@gmail.com>
2021-01-10 23:32:07 +0600

    UI done kinda

--- lib/screens/student_list_for_detail.dart
+++ lib/screens/student_list_for_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:smanage/screens/student_details.dart';
 import 'package:smanage/services/database.dart';
+import 'package:smanage/utils/constants.dart';
 // import 'package:date_time_format/date_time_format.dart';
 
 class StudentListForDetail extends StatelessWidget {
@@ -27,15 +28,28 @@ class StudentListForDetail extends StatelessWidget {
               final docs = snapshot.data.docs;
 
               return ListView.builder(
+                physics: BouncingScrollPhysics(),
                 itemCount: docs.length,
                 itemBuilder: (context, index) {
-                  return FlatButton(
-                    child: Text(docs[index].data()['name']),
-                    onPressed: () {
-                      Get.to(STDT(
-                        doc: docs[index],
-                      ));
-                    },
+                  return Container(
+                    margin: EdgeInsets.symmetric(vertical: 5),
+                    child: FlatButton(
+                      color: kDoneColor,
+                      height: kFlatButtonHeight,
+                      child: Row(
+                        children: [
+                          Text(
+                            docs[index].data()['name'],
+                            style: kTextStyle,
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        Get.to(STDT(
+                          doc: docs[index],
+                        ));
+                      },
+                    ),
                   );
                 },
               );
@@ -50,3 +64,62 @@ class StudentListForDetail extends StatelessWidget {
     );
   }
 }
+
+//  return Container(
+//                                             margin: EdgeInsets.symmetric(
+//                                                 vertical: 5),
+//                                             child: FlatButton(
+//                                               height: 50,
+
+//                                               child: Row(children: [
+//                                                 Expanded(
+//                                                   flex: 1,
+//                                                   child: Padding(
+//                                                     padding:
+//                                                         const EdgeInsets.only(
+//                                                       left: 15,
+//                                                       // right: 21,
+//                                                     ),
+//                                                     child: Text(
+//                                                       doc
+//                                                           .data()['roll']
+//                                                           .toString(),
+//                                                       style: kTextStyle,
+//                                                     ),
+//                                                   ),
+//                                                 ),
+//                                                 Expanded(
+//                                                   flex: 5,
+//                                                   child: Padding(
+//                                                     padding:
+//                                                         const EdgeInsets.only(
+//                                                       left: 15,
+//                                                     ),
+//                                                     child: Text(
+//                                                       doc.data()['name'],
+//                                                       overflow:
+//                                                           TextOverflow.ellipsis,
+//                                                       style: kTextStyle,
+//                                                     ),
+//                                                   ),
+//                                                 ),
+//                                                 Expanded(
+//                                                   flex: 1,
+//                                                   child: Icon(
+//                                                     snapshot.data.get(
+//                                                                 'attendant') ==
+//                                                             true
+//                                                         ? Icons.check
+//                                                         : Icons.close_rounded,
+//                                                     color: Colors.white,
+//                                                     size: 40,
+//                                                   ),
+//                                                 ),
+//                                               ]),
+//                                               color: snapshot.data
+//                                                           .get('attendant') ==
+//                                                       true
+//                                                   ? kDoneColor
+//                                                   : kUndoneColor,
+//                                             ),
+//                                           );
