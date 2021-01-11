22ffbbe0910edb2185ab8964c03862b985805362
chandra-deb <mail.chandradeb@gmail.com>
2021-01-11 20:35:18 +0600

    refined home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:smanage/controllers/roll_call_controller.dart';
 import 'package:smanage/screens/student_list_for_detail.dart';
 import 'package:smanage/screens/student_list_for_roll_call.dart';
-// import 'package:get/get.dart';
-// import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import 'package:smanage/services/auth.dart';
+
 import 'package:smanage/utils/constants.dart';
 import '../screens/add_student.dart';
 
@@ -51,13 +48,6 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     // addStudent(context);
-      //     Get.to(AddStudent());
-      //   },
-      //   child: Icon(Icons.add),
-      // ),
       appBar: AppBar(
         title: Text(_titles[_selectedIndex]),
         actions: [
@@ -113,13 +103,6 @@ class RollCallOf extends StatelessWidget {
                 clsNumber: 6,
               ));
             },
-            onLongPress: () {
-              Get.to(
-                StudentListForDetail(
-                  clsNumber: 6,
-                ),
-              );
-            },
             child: Container(
               padding: const EdgeInsets.all(8),
               child: Center(
@@ -137,13 +120,6 @@ class RollCallOf extends StatelessWidget {
               Get.to(StudentListForRC(
                 clsNumber: 7,
               ));
-            },
-            onLongPress: () {
-              Get.to(
-                StudentListForDetail(
-                  clsNumber: 7,
-                ),
-              );
             },
             child: Container(
               padding: const EdgeInsets.all(8),
@@ -188,13 +164,6 @@ class RollCallOf extends StatelessWidget {
                 clsNumber: 9,
               ));
             },
-            onLongPress: () {
-              Get.to(
-                StudentListForDetail(
-                  clsNumber: 9,
-                ),
-              );
-            },
             child: Container(
               padding: const EdgeInsets.all(8),
               child: Center(
@@ -235,13 +204,6 @@ class RollCallOf extends StatelessWidget {
               Get.to(StudentListForRC(
                 clsNumber: 11,
               ));
-            },
-            onLongPress: () {
-              Get.to(
-                StudentListForDetail(
-                  clsNumber: 11,
-                ),
-              );
             },
             child: Container(
               padding: const EdgeInsets.all(8),
@@ -286,7 +248,7 @@ class StudentDetailsOf extends StatelessWidget {
               padding: const EdgeInsets.all(8),
               child: Center(
                 child: Text(
-                  "Student Details of Class 6",
+                  "Details of Class 6",
                   style: kTextStyle.copyWith(fontSize: 30),
                   textAlign: TextAlign.center,
                 ),
@@ -306,7 +268,7 @@ class StudentDetailsOf extends StatelessWidget {
               padding: const EdgeInsets.all(8),
               child: Center(
                 child: Text(
-                  "Student Details of Class 7",
+                  "Details of Class 7",
                   style: kTextStyle.copyWith(fontSize: 30),
                   textAlign: TextAlign.center,
                 ),
@@ -326,7 +288,7 @@ class StudentDetailsOf extends StatelessWidget {
               padding: const EdgeInsets.all(8),
               child: Center(
                 child: Text(
-                  "Student Details of Class 8",
+                  "Details of Class 8",
                   style: kTextStyle.copyWith(fontSize: 30),
                   textAlign: TextAlign.center,
                 ),
@@ -346,7 +308,7 @@ class StudentDetailsOf extends StatelessWidget {
               padding: const EdgeInsets.all(8),
               child: Center(
                 child: Text(
-                  "Student Details of Class 9",
+                  "Details of Class 9",
                   style: kTextStyle.copyWith(fontSize: 30),
                   textAlign: TextAlign.center,
                 ),
@@ -364,7 +326,7 @@ class StudentDetailsOf extends StatelessWidget {
               padding: const EdgeInsets.all(8),
               child: Center(
                 child: Text(
-                  "Student Details of Class 10",
+                  "Details of Class 10",
                   style: kTextStyle.copyWith(fontSize: 30),
                   textAlign: TextAlign.center,
                 ),
@@ -384,7 +346,7 @@ class StudentDetailsOf extends StatelessWidget {
               padding: const EdgeInsets.all(8),
               child: Center(
                 child: Text(
-                  "Student Details of Class 11",
+                  "Details of Class 11",
                   style: kTextStyle.copyWith(fontSize: 30),
                   textAlign: TextAlign.center,
                 ),
