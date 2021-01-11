fd829aea2ee5d7c45dab2b76f6c0f2bada964666
chandra-deb <mail.chandradeb@gmail.com>
2021-01-11 21:47:52 +0600

    some theming

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:smanage/controllers/root_controller.dart';
+import 'package:smanage/utils/constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,12 +17,18 @@ class MyApp extends StatelessWidget {
       defaultTransition: Transition.cupertino,
       theme: ThemeData.light().copyWith(
         scaffoldBackgroundColor: Color(0xfff6f6f6),
-        accentColor: Colors.green.shade500,
+        accentColor: kDoneColor,
         appBarTheme: AppBarTheme(
           color: Colors.green.shade600,
         ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData()
-            .copyWith(backgroundColor: Colors.red),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData()
+            .copyWith(selectedItemColor: Colors.green.shade600),
+        primaryColor: Colors.green.shade600,
+        errorColor: kUndoneColor,
+        indicatorColor: kDoneColor,
+        cursorColor: kDoneColor,
+        textSelectionColor: Colors.green.shade100,
+        textSelectionHandleColor: kDoneColor,
       ),
       title: 'Student Manager',
       home: Root(),
