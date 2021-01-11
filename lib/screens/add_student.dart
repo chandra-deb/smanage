45d3ed1a4db338662214bc14d99bf4960fd1d8a8
chandra-deb <mail.chandradeb@gmail.com>
2021-01-11 16:22:30 +0600

    setup navigation base

--- lib/screens/add_student.dart
+++ lib/screens/add_student.dart
@@ -11,113 +11,105 @@ class AddStudent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Add Student'),
-        centerTitle: true,
-      ),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-        child: Obx(
-          () {
-            return SingleChildScrollView(
-              physics: BouncingScrollPhysics(),
-              child: Column(
-                children: [
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                        labelText: 'Name', errorText: _.studentNameErr.value),
-                    onChanged: (value) {
-                      _.getStudentNameErr(value);
-                    },
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                        labelText: 'Father\'s Name',
-                        errorText: _.fatherNameErr.value),
-                    onChanged: (value) {
-                      _.getFatherNameErr(value);
-                    },
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                        labelText: 'Mother\'s Name',
-                        errorText: _.motherNameErr.value),
-                    onChanged: (value) {
-                      _.getMotherNameErr(value);
-                    },
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                        labelText: 'School/College Name',
-                        errorText: _.schoolNameErr.value),
-                    onChanged: (value) {
-                      _.getSchoolNameErr(value);
-                    },
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                        labelText: 'Class', errorText: _.clsErr.value),
-                    onChanged: (value) {
-                      _.getClassErr(value);
-                    },
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                        labelText: 'Roll', errorText: _.rollErr.value),
-                    onChanged: (value) {
-                      _.getRollErr(value);
-                    },
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(
-                        labelText: 'Phone', errorText: _.studentPhoneErr.value),
-                    onChanged: (value) {
-                      _.getStudentPhoneErr(value);
-                    },
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(
-                        labelText: 'Father\'s Phone*',
-                        errorText: _.fatherPhoneErr.value),
-                    onChanged: (value) {
-                      _.getFatherPhoneErr(value);
-                    },
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(
-                        labelText: 'Mother\'s Phone*',
-                        errorText: _.motherPhoneErr.value),
-                    onChanged: (value) {
-                      _.getMotherPhoneErr(value);
-                    },
-                  ),
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                        labelText: 'Address', errorText: _.addressErr.value),
-                    onChanged: (value) {
-                      _.getAddressErr(value);
-                    },
-                  ),
-                  ElevatedButton(onPressed: _.button.value, child: Text('Add')),
-                  _.loading.value == true
-                      ? LinearProgressIndicator()
-                      : SizedBox()
-                ],
-              ),
-            );
-          },
-        ),
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Obx(
+        () {
+          return SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                      labelText: 'Name', errorText: _.studentNameErr.value),
+                  onChanged: (value) {
+                    _.getStudentNameErr(value);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                      labelText: 'Father\'s Name',
+                      errorText: _.fatherNameErr.value),
+                  onChanged: (value) {
+                    _.getFatherNameErr(value);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                      labelText: 'Mother\'s Name',
+                      errorText: _.motherNameErr.value),
+                  onChanged: (value) {
+                    _.getMotherNameErr(value);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                      labelText: 'School/College Name',
+                      errorText: _.schoolNameErr.value),
+                  onChanged: (value) {
+                    _.getSchoolNameErr(value);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Class', errorText: _.clsErr.value),
+                  onChanged: (value) {
+                    _.getClassErr(value);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Roll', errorText: _.rollErr.value),
+                  onChanged: (value) {
+                    _.getRollErr(value);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                      labelText: 'Phone', errorText: _.studentPhoneErr.value),
+                  onChanged: (value) {
+                    _.getStudentPhoneErr(value);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                      labelText: 'Father\'s Phone*',
+                      errorText: _.fatherPhoneErr.value),
+                  onChanged: (value) {
+                    _.getFatherPhoneErr(value);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                      labelText: 'Mother\'s Phone*',
+                      errorText: _.motherPhoneErr.value),
+                  onChanged: (value) {
+                    _.getMotherPhoneErr(value);
+                  },
+                ),
+                TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                      labelText: 'Address', errorText: _.addressErr.value),
+                  onChanged: (value) {
+                    _.getAddressErr(value);
+                  },
+                ),
+                ElevatedButton(onPressed: _.button.value, child: Text('Add')),
+                _.loading.value == true ? LinearProgressIndicator() : SizedBox()
+              ],
+            ),
+          );
+        },
       ),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,19 +8,34 @@ import 'package:smanage/screens/student_list_for_roll_call.dart';
 import 'package:smanage/services/auth.dart';
 import '../screens/add_student.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetOptions = <Widget>[RC(), RC(), AddStudent()];
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  List<String> _titles = ['Roll Call', 'Student Details', 'Add Student Form'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // addStudent(context);
-          Get.to(AddStudent());
-        },
-        child: Icon(Icons.add),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     // addStudent(context);
+      //     Get.to(AddStudent());
+      //   },
+      //   child: Icon(Icons.add),
+      // ),
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text(_titles[_selectedIndex]),
         actions: [
           FlatButton(
             onPressed: () {
@@ -30,160 +45,179 @@ class Home extends StatelessWidget {
           )
         ],
       ),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: GridView.count(
-          physics: BouncingScrollPhysics(),
-          primary: false,
-          padding: const EdgeInsets.all(20),
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-          crossAxisCount: 2,
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                Get.to(StudentListForRC(
+      body: _widgetOptions[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark_outline), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.class__rounded), label: 'Show'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined), label: 'Add Student'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+      ),
+    );
+  }
+}
+
+class RC extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: GridView.count(
+        physics: BouncingScrollPhysics(),
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              Get.to(StudentListForRC(
+                clsNumber: 6,
+              ));
+            },
+            onLongPress: () {
+              Get.to(
+                StudentListForDetail(
                   clsNumber: 6,
-                ));
-              },
-              onLongPress: () {
-                Get.to(
-                  StudentListForDetail(
-                    clsNumber: 6,
-                  ),
-                );
-              },
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                child: Center(
-                  child: const Text(
-                    "Class 6",
-                    style: TextStyle(fontSize: 35),
-                  ),
                 ),
-                color: Colors.teal[100],
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Center(
+                child: const Text(
+                  "Class 6",
+                  style: TextStyle(fontSize: 35),
+                ),
               ),
+              color: Colors.teal[100],
             ),
-            GestureDetector(
-              onTap: () {
-                Get.to(StudentListForRC(
+          ),
+          GestureDetector(
+            onTap: () {
+              Get.to(StudentListForRC(
+                clsNumber: 7,
+              ));
+            },
+            onLongPress: () {
+              Get.to(
+                StudentListForDetail(
                   clsNumber: 7,
-                ));
-              },
-              onLongPress: () {
-                Get.to(
-                  StudentListForDetail(
-                    clsNumber: 7,
-                  ),
-                );
-              },
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                child: Center(
-                  child: const Text(
-                    "Class 7",
-                    style: TextStyle(fontSize: 35),
-                  ),
                 ),
-                color: Colors.teal[100],
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Center(
+                child: const Text(
+                  "Class 7",
+                  style: TextStyle(fontSize: 35),
+                ),
               ),
+              color: Colors.teal[100],
             ),
-            GestureDetector(
-              onTap: () {
-                Get.to(StudentListForRC(
+          ),
+          GestureDetector(
+            onTap: () {
+              Get.to(StudentListForRC(
+                clsNumber: 8,
+              ));
+            },
+            onLongPress: () {
+              Get.to(
+                StudentListForDetail(
                   clsNumber: 8,
-                ));
-              },
-              onLongPress: () {
-                Get.to(
-                  StudentListForDetail(
-                    clsNumber: 8,
-                  ),
-                );
-              },
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                child: Center(
-                  child: const Text(
-                    "Class 8",
-                    style: TextStyle(fontSize: 35),
-                  ),
                 ),
-                color: Colors.teal[100],
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Center(
+                child: const Text(
+                  "Class 8",
+                  style: TextStyle(fontSize: 35),
+                ),
               ),
+              color: Colors.teal[100],
             ),
-            GestureDetector(
-              onTap: () {
-                Get.to(StudentListForRC(
+          ),
+          GestureDetector(
+            onTap: () {
+              Get.to(StudentListForRC(
+                clsNumber: 9,
+              ));
+            },
+            onLongPress: () {
+              Get.to(
+                StudentListForDetail(
                   clsNumber: 9,
-                ));
-              },
-              onLongPress: () {
-                Get.to(
-                  StudentListForDetail(
-                    clsNumber: 9,
-                  ),
-                );
-              },
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                child: Center(
-                  child: const Text(
-                    "Class 9",
-                    style: TextStyle(fontSize: 35),
-                  ),
                 ),
-                color: Colors.teal[100],
-              ),
-            ),
-            GestureDetector(
-              onTap: () async {
-                await Get.to(StudentListForRC(
-                  clsNumber: 10,
-                ));
-              },
-              onLongPress: () {
-                Get.to(StudentListForDetail(
-                  clsNumber: 10,
-                ));
-              },
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                child: Center(
-                  child: const Text(
-                    "Class 10",
-                    style: TextStyle(fontSize: 35),
-                  ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Center(
+                child: const Text(
+                  "Class 9",
+                  style: TextStyle(fontSize: 35),
                 ),
-                color: Colors.teal[100],
               ),
+              color: Colors.teal[100],
             ),
-            GestureDetector(
-              onTap: () {
-                Get.to(StudentListForRC(
+          ),
+          GestureDetector(
+            onTap: () async {
+              await Get.to(StudentListForRC(
+                clsNumber: 10,
+              ));
+            },
+            onLongPress: () {
+              Get.to(StudentListForDetail(
+                clsNumber: 10,
+              ));
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Center(
+                child: const Text(
+                  "Class 10",
+                  style: TextStyle(fontSize: 35),
+                ),
+              ),
+              color: Colors.teal[100],
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Get.to(StudentListForRC(
+                clsNumber: 11,
+              ));
+            },
+            onLongPress: () {
+              Get.to(
+                StudentListForDetail(
                   clsNumber: 11,
-                ));
-              },
-              onLongPress: () {
-                Get.to(
-                  StudentListForDetail(
-                    clsNumber: 11,
-                  ),
-                );
-              },
-              child: Container(
-                padding: const EdgeInsets.all(8),
-                child: Center(
-                  child: const Text(
-                    "Class 11",
-                    style: TextStyle(fontSize: 35),
-                  ),
                 ),
-                color: Colors.teal[100],
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: Center(
+                child: const Text(
+                  "Class 11",
+                  style: TextStyle(fontSize: 35),
+                ),
               ),
+              color: Colors.teal[100],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
