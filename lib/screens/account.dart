14eea028ae5fb996d391afbda35484147f57c258
Chandra Deb <mail.chandradeb@gmail.com>
2021-01-18 19:55:38 +0600

    implemented class deletion functionalities

--- lib/screens/account.dart
+++ lib/screens/account.dart
@@ -1,23 +1,110 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:prompt_dialog/prompt_dialog.dart';
+import 'package:smanage/models/teacher.dart';
 import 'package:smanage/services/auth.dart';
+import 'package:smanage/services/database.dart';
+import 'package:toast/toast.dart';
 
 class Account extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Auth _auth = Auth();
+    final DocumentReference teacherData =
+        DB().store.collection('teachers').doc(_auth.teacherUID);
     print(_auth.name);
     return Container(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('${_auth.name.toString()}'),
-          Text('Class 7 monthly bill'),
-          Text('Class 8 monthly bill'),
-          Text('Class 9 monthly bill'),
-          Text('Class 10 monthly bill'),
-          Text('Class 11 monthly bill'),
-          Text('Class 12 monthly bill')
+          StreamBuilder(
+            stream: teacherData.snapshots(),
+            // ignore: missing_return
+            builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+              // switch (snapshot.connectionState) {
+              //   case ConnectionState.done:
+              //     return Text('${snapshot.data['classes']}');
+              //   case ConnectionState.waiting:
+              //     return CircularProgressIndicator();
+              //   case ConnectionState.none:
+              //     return Text('Something went wrong!');
+              //   case ConnectionState.active:
+              //   // TODO: Handle this case.
+              // }
+
+              if (snapshot.hasData) {
+                return Container(
+                  color: Colors.green.shade100,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text('Classes you have:'),
+                          ...snapshot.data['classes'].map((t) => Container(
+                              padding: EdgeInsets.all(5),
+                              child: Text(t.toString() + ',')))
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          FlatButton(
+                            child: Text('Add Class'),
+                            onPressed: () {},
+                          ),
+                          FlatButton(
+                            child: Text('Delete Class'),
+                            onPressed: () async {
+                              String result = await alertWithInput(context);
+                              // print(teacherData.path);
+                              print(snapshot.data['classes']
+                                  .contains(int.tryParse(result)));
+                              if (snapshot.data['classes']
+                                      .contains(int.tryParse(result)) ==
+                                  true) {
+                                List data = snapshot.data['classes'] as List;
+                                int index = data.indexOf(int.parse(result));
+                                var updatedData = data.removeAt(index);
+                                print(data);
+                                print(updatedData);
+                                try {
+                                  await teacherData.update({'classes': data});
+                                } on Exception catch (e) {
+                                  Toast.show(
+                                    'Something Went Wrong!',
+                                    context,
+                                    duration: Toast.LENGTH_LONG,
+                                    gravity: Toast.BOTTOM,
+                                  );
+                                }
+                              }
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          )
         ],
+      ),
+    );
+  }
+
+  Future<String> alertWithInput(BuildContext context) async {
+    return await prompt(
+      context,
+      title: Text('Do you confirm?'),
+      hintText: 'Class',
+      textOK: Text(
+        'Confirm',
       ),
     );
   }
