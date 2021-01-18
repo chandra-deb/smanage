ecf96c6c263609f905187d2f64ec67987c86c126
Chandra Deb <chandra-deb@users.noreply.github.com>
2021-01-18 13:08:01 +0600

    account page now shows teacher name

--- lib/screens/account.dart
+++ lib/screens/account.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:smanage/services/auth.dart';
 
 class Account extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Auth _auth = Auth();
+    print(_auth.name);
     return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text('Class 6 monthly bill'),
+          Text('${_auth.name.toString()}'),
           Text('Class 7 monthly bill'),
           Text('Class 8 monthly bill'),
           Text('Class 9 monthly bill'),
