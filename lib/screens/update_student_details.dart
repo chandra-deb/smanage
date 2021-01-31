9170a58a42cc40a8fe98825f6da947b6d40bd425
chandra-deb <mail.chandradeb@gmail.com>
2021-01-31 15:37:11 +0600

    student details ref

--- lib/screens/update_student_details.dart
+++ lib/screens/update_student_details.dart
@@ -14,6 +14,7 @@ class UpdateStudentDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final UpdateStudentController _ = UpdateStudentController(
+      ref: currentDetail.ref,
       studentName: currentDetail.name,
       fatherName: currentDetail.fatherName,
       motherName: currentDetail.motherName,
